e7a4ff00cc3e33393f1502e09d7899d0e91c9a51
Thiago Santos <thiago.santos@maxmilhas.com.br>
2022-12-14 10:01:40 -0300

    fix: fixing contract

--- src/ordered-map-declaration.ts
+++ src/ordered-map-declaration.ts
@@ -5,7 +5,7 @@ import {
 	Mapper,
 	Reducer,
 } from '@codibre/fluent-iterable';
-import { OrderedMap } from 'js-sdsl';
+import { BinarySearchTree } from './binary-search-tree';
 import { toOrderedMap } from './to-ordered-map';
 import { toOrderedMapAsync } from './to-ordered-map-async';
 
@@ -14,97 +14,97 @@ declare module '@codibre/fluent-iterable' {
 		toOrderedMap<K>(
 			key: Mapper<T, K>,
 			cmp?: (x: K, y: K) => number,
-		): OrderedMap<K, T>;
+		): BinarySearchTree<K, T>;
 		toOrderedMap<K extends keyof T>(
 			key: K,
 			cmp?: (x: T[K], y: T[K]) => number,
-		): OrderedMap<T[K], T>;
+		): BinarySearchTree<T[K], T>;
 		toOrderedMap<K, V>(
 			key: Mapper<T, K>,
 			value: Mapper<T, V>,
 			cmp?: (x: K, y: K) => number,
 			reducer?: Reducer<T, V>,
-		): OrderedMap<K, V>;
+		): BinarySearchTree<K, V>;
 		toOrderedMap<K extends keyof T, V>(
 			key: K,
 			value: Mapper<T, V>,
 			cmp?: (x: T[K], y: T[K]) => number,
 			reducer?: Reducer<T, V>,
-		): OrderedMap<T[K], V>;
+		): BinarySearchTree<T[K], V>;
 		toOrderedMap<K, V extends keyof T>(
 			key: Mapper<T, K>,
 			value: V,
 			cmp?: (x: K, y: K) => number,
-		): OrderedMap<K, T[V]>;
+		): BinarySearchTree<K, T[V]>;
 		toOrderedMap<K extends keyof T, V extends keyof T>(
 			key: K,
 			value: V,
 			cmp?: (x: T[K], y: T[K]) => number,
-		): OrderedMap<T[K], T[V]>;
+		): BinarySearchTree<T[K], T[V]>;
 
 		toOrderedMapAsync<K>(
 			key: Mapper<T, K>,
 			cmp?: (x: K, y: K) => number,
-		): Promise<OrderedMap<K, T>>;
+		): Promise<BinarySearchTree<K, T>>;
 		toOrderedMapAsync<K extends keyof T>(
 			key: K,
 			cmp?: (x: T[K], y: T[K]) => number,
-		): Promise<OrderedMap<T[K], T>>;
+		): Promise<BinarySearchTree<T[K], T>>;
 		toOrderedMapAsync<K, V>(
 			key: Mapper<T, K>,
 			value: Mapper<T, V>,
 			cmp?: (x: K, y: K) => number,
 			reducer?: Reducer<T, V>,
-		): Promise<OrderedMap<K, V>>;
+		): Promise<BinarySearchTree<K, V>>;
 		toOrderedMapAsync<K extends keyof T, V>(
 			key: K,
 			value: Mapper<T, V>,
 			cmp?: (x: T[K], y: T[K]) => number,
 			reducer?: Reducer<T, V>,
-		): Promise<OrderedMap<T[K], V>>;
+		): Promise<BinarySearchTree<T[K], V>>;
 		toOrderedMapAsync<K, V extends keyof T>(
 			key: Mapper<T, K>,
 			value: V,
 			cmp?: (x: K, y: K) => number,
-		): Promise<OrderedMap<K, T[V]>>;
+		): Promise<BinarySearchTree<K, T[V]>>;
 		toOrderedMapAsync<K extends keyof T, V extends keyof T>(
 			key: K,
 			value: V,
 			cmp?: (x: T[K], y: T[K]) => number,
-		): Promise<OrderedMap<T[K], T[V]>>;
+		): Promise<BinarySearchTree<T[K], T[V]>>;
 	}
 
 	interface FluentAsyncIterable<T> {
 		toOrderedMap<K>(
 			key: Mapper<T, K>,
 			cmp?: (x: K, y: K) => number,
-		): Promise<OrderedMap<K, T>>;
+		): Promise<BinarySearchTree<K, T>>;
 		toOrderedMap<K extends keyof T>(
 			key: K,
 			cmp?: (x: T[K], y: T[K]) => number,
-		): Promise<OrderedMap<T[K], T>>;
+		): Promise<BinarySearchTree<T[K], T>>;
 		toOrderedMap<K, V>(
 			key: Mapper<T, K>,
 			value: Mapper<T, V>,
 			cmp?: (x: K, y: K) => number,
 			reducer?: Reducer<T, V>,
-		): Promise<OrderedMap<K, V>>;
+		): Promise<BinarySearchTree<K, V>>;
 		toOrderedMap<K extends keyof T, V>(
 			key: K,
 			value: Mapper<T, V>,
 			cmp?: (x: T[K], y: T[K]) => number,
 			reducer?: Reducer<T, V>,
-		): Promise<OrderedMap<T[K], V>>;
+		): Promise<BinarySearchTree<T[K], V>>;
 		toOrderedMap<K, V extends keyof T>(
 			key: Mapper<T, K>,
 			value: V,
 			cmp?: (x: K, y: K) => number,
-		): Promise<OrderedMap<K, T[V]>>;
+		): Promise<BinarySearchTree<K, T[V]>>;
 		toOrderedMap<K extends keyof T, V extends keyof T>(
 			key: K,
 			value: V,
 			cmp?: (x: T[K], y: T[K]) => number,
-		): Promise<OrderedMap<T[K], T[V]>>;
+		): Promise<BinarySearchTree<T[K], T[V]>>;
 	}
 }
 
